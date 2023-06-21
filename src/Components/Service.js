04219e9df14fc5a6e9d0a2d221992fa2751c69ce
Abrar Islam <abrarr18@vt.edu>
2023-06-20 23:42:50 -0400

    Upate on project page

--- src/Components/Service.js
+++ src/Components/Service.js
@@ -5,7 +5,17 @@ import transaction_classifer from "../img/transaction_classify.jpg";
 import bookstore_homepage from "../img/bookstore_homepage.PNG";
 import topic_modeling from "../img/topic-modeling.png";
 import photo_gallery from "../img/photo-gallery.jpg";
-import textbook_app from "../img/textbook_app.JPG"
+import textbook_app from "../img/textbook_app.JPG";
+import python_logo from "../img/python_logo.png";
+import tensorflow_logo from "../img/tensorflow_logo.png";
+import java_logo from "../img/Java-Logo.png"; 
+import vuejs_logo from "../img/Vue.js_Logo.png"; 
+import mysql_logo from "../img/MySQL-Logo.png"; 
+import react_logo from "../img/React-icon.png"; 
+import aws_logo from "../img/Amazon-Web-Services-AWS-Logo.png"; 
+import android_logo from "../img/Android-Logo.png"; 
+import as_logo from "../img/Android_Studio_Icon_3.6.svg.png";
+import kotlin_logo from "../img/Kotlin_Icon.svg.png"; 
 
 function Service() {
   return (
@@ -24,7 +34,10 @@ function Service() {
                 <div className="img-card iCard-style1">
                   <div className="card-content">
                     <div className="card-image">
-                      <img src={traffic_sign_recognizer} alt="Traffic Sign Recognizer" />
+                      <img
+                        src={traffic_sign_recognizer}
+                        alt="Traffic Sign Recognizer"
+                      />
                     </div>
 
                     <div className="card-text">
@@ -41,6 +54,10 @@ function Service() {
                           Pixabay
                         </a>
                       </p>
+                      <div className="tool-icons">
+                        <img className="small_logo" src={python_logo} alt="Python logo" />
+                        <img className="small_logo" src={tensorflow_logo} alt="Tensorflow logo" />
+                      </div>
                     </div>
                   </div>
 
@@ -64,7 +81,6 @@ function Service() {
                       <p>
                         Lorem Ipsum is simply dummy text of the printing and
                         typesetting industry. Image by{" "}
-                      
                         <a
                           href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3461451"
                           style={{ color: "#795548" }}
@@ -72,6 +88,11 @@ function Service() {
                           Pixabay
                         </a>
                       </p>
+                      <div className="tool-icons">
+                        <img className="small_logo" src={vuejs_logo} alt="VUE.js logo" />
+                        <img className="big_logo" src={java_logo} alt="Javalogo" />
+                        <img className="big_logo" src={mysql_logo} alt="MySQL logo" />
+                      </div>
                     </div>
                   </div>
 
@@ -87,7 +108,10 @@ function Service() {
                 <div className="img-card iCard-style1">
                   <div className="card-content">
                     <div className="card-image">
-                      <img src={transaction_classifer} alt="Transaction Classifier" />
+                      <img
+                        src={transaction_classifer}
+                        alt="Transaction Classifier"
+                      />
                     </div>
 
                     <div className="card-text">
@@ -95,7 +119,6 @@ function Service() {
                       <p>
                         Lorem Ipsum is simply dummy text of the printing and
                         typesetting industry. Image by{" "}
-                        
                         <a
                           href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3461451"
                           style={{ color: "#795548" }}
@@ -103,6 +126,10 @@ function Service() {
                           Pixabay
                         </a>
                       </p>
+                      <div className="tool-icons">
+                        <img className="small_logo" src={python_logo} alt="Python logo" />
+                        <img className="small_logo" src={tensorflow_logo} alt="Tensorflow logo" />
+                      </div>
                     </div>
                   </div>
 
@@ -118,7 +145,7 @@ function Service() {
                 <div className="img-card iCard-style1">
                   <div className="card-content">
                     <div className="card-image">
-                      <img src={topic_modeling} alt="IOP Asset Tracker"/>
+                      <img src={topic_modeling} alt="IOP Asset Tracker" />
                     </div>
 
                     <div className="card-text">
@@ -126,7 +153,6 @@ function Service() {
                       <p>
                         Lorem Ipsum is simply dummy text of the printing and
                         typesetting industry. Image by{" "}
-                        
                         <a
                           href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3461451"
                           style={{ color: "#795548" }}
@@ -134,6 +160,11 @@ function Service() {
                           Pixabay
                         </a>
                       </p>
+                      <div className="tool-icons">
+                        <img className="small_logo" src={python_logo} alt="Python logo" />
+                        <img className="small_logo" src={react_logo} alt="React logo" />
+                        <img className="big_logo" src={aws_logo} alt="AWS logo" />
+                      </div> 
                     </div>
                   </div>
 
@@ -149,7 +180,7 @@ function Service() {
                 <div className="img-card iCard-style1">
                   <div className="card-content">
                     <div className="card-image">
-                      <img src={photo_gallery} alt="Fancy Gallery"/>
+                      <img src={photo_gallery} alt="Fancy Gallery" />
                     </div>
 
                     <div className="card-text">
@@ -157,7 +188,6 @@ function Service() {
                       <p>
                         Lorem Ipsum is simply dummy text of the printing and
                         typesetting industry. Image by{" "}
-                        
                         <a
                           href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3461451"
                           style={{ color: "#795548" }}
@@ -165,6 +195,11 @@ function Service() {
                           Pixabay
                         </a>
                       </p>
+                      <div className="tool-icons">
+                        <img className="small_logo" src={kotlin_logo} alt="Python logo" />
+                        <img className="big_logo" src={android_logo} alt="React logo" />
+                        <img className="small_logo" src={as_logo} alt="AWS logo" />
+                      </div>
                     </div>
                   </div>
 
@@ -188,7 +223,6 @@ function Service() {
                       <p>
                         Lorem Ipsum is simply dummy text of the printing and
                         typesetting industry. Image by{" "}
-                        
                         <a
                           href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=3461451"
                           style={{ color: "#795548" }}
@@ -196,6 +230,11 @@ function Service() {
                           Pixabay
                         </a>
                       </p>
+                      <div className="tool-icons">
+                        <img className="big_logo" src={java_logo} alt="Python logo" />
+                        <img className="big_logo" src={android_logo} alt="React logo" />
+                        <img className="small_logo" src={as_logo} alt="AWS logo" />
+                      </div>
                     </div>
                   </div>
 
