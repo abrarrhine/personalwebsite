cd22f695b852474f1068af0e2dc81ab599381f70
Abrar Islam <abrarr18@vt.edu>
2023-07-04 00:41:49 -0400

    Added resume icon button

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,8 +1,10 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import "./About.css";
 import about__img from "../img/gradpic_pylons22.jpeg";
 import resume from "../img/Abrar_Rhine_resume.pdf";
+import resume_icon from "../img/cv.png";
 
 function About() {
   return (
@@ -15,16 +17,16 @@ function About() {
             <div className="col__22">
               <h1 className="about__heading sweet">About Me</h1>
               <div className=".about__meta">
+                <p className="about__text p__color">Greetings!</p>
                 <p className="about__text p__color">
-                  Greetings! I'm Abrar Rhine, a passionate Software Engineer,
-                  holding a BSc and MEng degree in Computer Science from
-                  Virginia Tech. I commenced my professional journey at Raytheon
-                  Technologies as an Intern, paving the way for a full-fledged
-                  role as a Software Developer at Systems Planning and Analysis
-                  Inc. I now hold the title of Sr. Software Development Engineer
-                  at Raytheon Technologies, leveraging my academic foundation
-                  and industry experience to drive innovative software
-                  solutions.
+                  I'm Abrar Rhine, a passionate Software Engineer, holding a BSc
+                  and MEng degree in Computer Science from Virginia Tech. I
+                  commenced my professional journey at Raytheon Technologies as
+                  an Intern, paving the way for a full-fledged role as a
+                  Software Developer at Systems Planning and Analysis Inc. I now
+                  hold the title of Sr. Software Development Engineer at
+                  Raytheon Technologies, leveraging my academic foundation and
+                  industry experience to drive innovative software solutions.
                 </p>
 
                 <p className="about__text p__color">
@@ -36,16 +38,28 @@ function About() {
                   software development projects.
                 </p>
 
-                <div className="about__button d__flex align__items__center">
-                  <a
-                    className="about resumebutton pointer"
-                    href={resume}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    View/Download My Resume
-                  </a>
-                </div>
+                <p className="about__text p__color">
+                  Passionate about Robotics, AI/ML, marketing, and sales, I am
+                  planning to further my knowledge by pursuing a PhD in Robotics
+                  and an MBA. My goal is to utilize these technical and business
+                  insights to enhance lives and stimulate innovation. I aim to
+                  contribute to a progressive, technology-driven world,
+                  fostering an environment that encourages creative
+                  problem-solving and simplifies life through advanced
+                  technologies.
+                </p>
+
+                <p className="about__text p__color">
+                  View/download my resume:
+                </p>
+                <a
+                  href={resume}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="resume-button"
+                >
+                  <img src={resume_icon} alt="CV Icon" />
+                </a>
               </div>
             </div>
             <div className="col__23">
