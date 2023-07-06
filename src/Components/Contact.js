81f2e465057ca5ec6eee6883e03403faadcb6d78
Abrar Islam <abrarr18@vt.edu>
2023-07-06 03:04:57 -0400

    Progress on Contact me page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Contact.css";
 import contactimg from "../img/ben_on_bench.JPG";
-import twtr_icon from "../img/twittericon.png"; 
-import yt_icon from "../img/yputubeicon.png"; 
-import discord_icon from "../img/discordicon.png"; 
-import gmail_icon from "../img/gmailicon.png"; 
-import lin_icon from "../img/linkedinIcon.png"; 
-import github_icon from "../img/githubicon.png"; 
+import twtr_icon from "../img/twittericon.png";
+import yt_icon from "../img/yputubeicon.png";
+import gmail_icon from "../img/gmailicon.png";
+import lin_icon from "../img/linkedinIcon.png";
+import github_icon from "../img/githubicon.png";
+import insta_icon from "../img/instagramicon.png";
 
 function Contact() {
   return (
@@ -49,7 +49,59 @@ function Contact() {
               </button>
             </div>
             <div className="social-media">
+              <a
+                href="https://www.twitter.com"
+                target="_blank"
+                rel="noreferrer"
+                title="Twitter account"
+              >
+                <img src={twtr_icon} alt="CV Icon" />
+              </a>
 
+              <a
+                href="https://www.linkedin.com/in/abrarrhine/"
+                target="_blank"
+                rel="noreferrer"
+                title="LinkedIn profile"
+              >
+                <img src={lin_icon} alt="CV Icon" />
+              </a>
+
+              <a
+                href="https://www.twitter.com"
+                target="_blank"
+                rel="noreferrer"
+                title="YouTube channel"
+              >
+                <img src={yt_icon} alt="CV Icon" />
+              </a>
+
+              <a
+                href="https://www.twitter.com"
+                target="_blank"
+                rel="noreferrer"
+                title="Github profile"
+              >
+                <img src={github_icon} alt="CV Icon" />
+              </a>
+
+              <a
+                href="https://www.twitter.com"
+                target="_blank"
+                rel="noreferrer"
+                title="Instagram profile"
+              >
+                <img src={insta_icon} alt="CV Icon" />
+              </a>
+
+              <a
+                href="mailto:abrarrhine@gmail.com"
+                target="_blank"
+                rel="noreferrer"
+                title="Send Email"
+              >
+                <img src={gmail_icon} alt="Email Icon" />
+              </a>
             </div>
           </div>
           <div className="col__2">
